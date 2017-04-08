ce774b351c908eb29b88bc0ff0e53e7c50ad4d77
xenogonx <pablez@wp.pl>
2017-04-09 01:08:32 +0200

    new_metod

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ function Telefon(marka, cena, kolor, system) {
    this.cena = cena;
    this.kolor = kolor;
    this.system = system;
+   this.telModel = function (){
+      console.log("marka: " + this.marka);
+   }
+   this.telModel();
 }
 Telefon.prototype.printInfo = function(){
    console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", a cena to " + this.cena +  ", system operacyjny to " + this.system + ".");
@@ -11,6 +15,7 @@ Telefon.prototype.printInfo = function(){
 var SamsungGalaxyS6 = new Telefon("Samsung", 1500, "srebrny", "Android 5.0");
 var iPhone6s = new Telefon("Apple", 2250, "srebrny", "iOS");
 var OnePlusOne = new Telefon("OnePlus", 1500, "srebrny", "android 5.0");
+
 
 SamsungGalaxyS6.printInfo();
 iPhone6s.printInfo();
